4b470a9287fbafc4e3984da4396a868e23522f15
Jean Lange <jean.f.lange@gmail.com>
2023-06-12 21:33:21 -0400

    nametags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Tag({name, color}) {
+  return (
+    <div className="tag">
+      <div className="greeting" style={{backgroundColor: color}}>Hello, my name is</div>
+      <div className="name">{name}</div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello, world!</h1>
+      <div className="tags">
+        <Tag name="Jean" color="blue" />
+        <Tag name="John" color="red" />
+        <Tag name="Sebastian" color="purple" />
+        <Tag name="Blorf" color="green" />
+      </div>
     </div>
   );
 }
