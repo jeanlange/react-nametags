09a200795c2ede4b8cc198fcf004f1638d1d5ece
Jean Lange <jean.f.lange@gmail.com>
2023-06-13 22:09:16 -0400

    put input box that types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Tag({name, color}) {
   return (
@@ -11,15 +12,22 @@ function Tag({name, color}) {
 }
 
 function App() {
+  const [newName, setNewName] = useState("");
   const colors = ["blue", "red", "green", "purple"]
   const names = ["Jean", "John", "Scott", "Bernadina-Bess", "Waldo", "Blorf"]
   const tags = names.map(name => {
     const color = colors[Math.floor(Math.random() * colors.length)];
     return <Tag name={name} color={color} />;
   })
+
+  function handleType(e) {
+    setNewName(e.target.value);
+  }
   return (
     <div>
       <h1>hello, world!</h1>
+      <input type="text" placeholder="A name?" value={newName} onChange={handleType}></input>
+      <button>Make tag!</button>
       <div className="tags">
         {tags}
       </div>
