162170284fdcf8a2b6bb41bb67caa8d0a8cf1ed3
Jean Lange <jean.f.lange@gmail.com>
2023-06-13 22:34:09 -0400

    colors are set once they start

--- src/App.js
+++ src/App.js
@@ -13,27 +13,36 @@ function Tag({name, color}) {
 
 function App() {
   const [newName, setNewName] = useState("");
-  const [names, setNames] = useState(["Jean",  "John", "Scott", "Bernadina-Bess", "Waldo", "Blorf"])
-  const colors = ["blue", "red", "green", "purple"]
-  const tags = names.map(name => {
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    return <Tag name={name} color={color} />;
-  })
+  const [names, setNames] = useState([
+    {name: "Jean",  color: randomColor()},
+    {name: "John", color: randomColor()}])
 
+  const tags = names.map(name => {
+    return <Tag name={name.name} color={name.color} />;
+  })
+  function randomColor() {
+    const colors = ["blue", "red", "green", "purple"]
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
   function handleType(e) {
     setNewName(e.target.value);
   }
   function newTag() {
     let newNames = names.slice();
-    newNames.unshift(newName);
+    newNames.unshift({name: newName, color: randomColor()});
     setNames(newNames);
     setNewName("");
+  }
+  function maybeNewTag(e) {
+    if (e.which === 13) {
+      newTag();
+    }
   }
   return (
     <div>
       <h1>hello, world!</h1>
       <input type="text" placeholder="A name?" value={newName} onChange={handleType}></input>
-      <button onClick={newTag}>Make tag!</button>
+      <button onClick={newTag} onKeyPress={maybeNewTag}>Make tag!</button>
       <div className="tags">
         {tags}
       </div>
